4d155e1816e99c17b4d57fe1e600da4e931fe19e
gerencsermate <gerencsermate02@gmail.com>
2026-03-20 22:11:46 +0100

    initialize file uploader

---
+++ file-uploader/Readme.md
@@ -0,0 +1,3 @@
+# File uploader service
+
+This service will host the ui and handle file uploading.
---
+++ file-uploader/app/main.py
@@ -0,0 +1,16 @@
+import os
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+NAME = os.getenv("USERNAME", "")
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "name": NAME})
+
